b7384fd946bc9decc23b1196c0c9badb7b5420ca
bobverity <r.verity@imperial.ac.uk>
2020-03-09 12:44:21 +0000

    added raw data

---
+++ process_mcmc_data.R
@@ -0,0 +1,13 @@
+
+# process_mcmc_data.R
+#
+# Author: Bob Verity
+# Date: 2020-03-09
+#
+# Purpose:
+# Read in raw China deaths and international data. Process data into simpler
+# form and save into outputs folder.
+#
+# ------------------------------------------------------------------
+
+
